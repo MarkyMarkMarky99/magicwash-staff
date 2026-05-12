af3d1c2b5bb1b74d353b7c16e6a74c15a2d359b7
Mark <gingjoknoi@gmail.com>
2026-05-12 20:33:12 +0700

    fix: correct Firebase storage bucket name

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey:            'AIzaSyBxDx2bEZD-sww2uo_gzN93tx9dTRcBS-M',
   authDomain:        'magicwashlaundry-a50ca.firebaseapp.com',
   projectId:         'magicwashlaundry-a50ca',
-  storageBucket:     'magicwash-db',
+  storageBucket:     'magicwashlaundry-a50ca.firebasestorage.app',
   messagingSenderId: '967978406004',
   appId:             '1:967978406004:web:d3552249e55dff9b0d8121',
 }
